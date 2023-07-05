e4ce7bdf4cd93683b97fb3c27e67e5a233f1a268
andishe <a.abbasian@pixaldo.com>
2023-07-05 20:25:20 +0330

    init icon image component

---
+++ src/components/layout/Header/IconImage.tsx
@@ -0,0 +1,10 @@
+import avatarPng from '@assets/pictures/Avatar.png'
+const IconImage = () => {
+  return (
+    <div>
+      <img src={avatarPng}></img>
+    </div>
+  )
+}
+
+export default IconImage
