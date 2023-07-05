fd4e09410ddcad3a91ced37d877217733678898d
andishe <a.abbasian@pixaldo.com>
2023-07-05 17:08:27 +0330

    updated header components

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,10 @@
+import Navbar from '@components/Header/Navbar'
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className='py-4 px-8 border-b'>
+      <Navbar />
+    </div>
+  )
 }
 
 export default Header
