a8b21b4d81a5d2fc6de654bdda0972bb8d161cb1
andishe <a.abbasian@pixaldo.com>
2023-07-06 18:38:57 +0330

    updated imporvement of navbar styling props

--- src/components/layout/Header/Navbar.tsx
+++ src/components/layout/Header/Navbar.tsx
@@ -22,6 +22,7 @@ const Navbar = () => {
             title={itm.title}
             key={itm.title}
             onClickAction={() => navigate(itm.path)}
+            className="text-secondary"
           />
         ))}
       </div>
