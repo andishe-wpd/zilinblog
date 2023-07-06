c8e2bd249af794db2fa24f7c5e01e8add7680942
andishe <a.abbasian@pixaldo.com>
2023-07-06 19:14:14 +0330

    updated post card gap and styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import PostCard from '@components/PostCards/PostCard'
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-6 pb-12">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-6 pb-12 max-w-[1600px] mx-auto">
       <PostCard />
       <PostCard />
       <PostCard />
