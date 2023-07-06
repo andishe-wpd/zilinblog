befae23255e26fc9426c6e2b17c65eaa9bc230e5
andishe <a.abbasian@pixaldo.com>
2023-07-06 19:22:23 +0330

    updated single card rendering static

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,11 @@
 import PostCard from '@components/PostCards/PostCard'
+import PostCards from '@components/PostCards/PostCards'
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-6 pb-12 max-w-[1600px] mx-auto">
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-      <PostCard />
-    </div>
+    <>
+      <PostCards />
+    </>
   )
 }
 
