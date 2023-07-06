6cd10bae637178ef7982ba976424b3ceae0cdd84
andishe <a.abbasian@pixaldo.com>
2023-07-06 18:40:00 +0330

    added static homepage temp

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,18 @@
+import PostCard from '@components/PostCards/PostCard'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 pt-6 pb-12">
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+      <PostCard />
+    </div>
+  )
 }
 
 export default Home
