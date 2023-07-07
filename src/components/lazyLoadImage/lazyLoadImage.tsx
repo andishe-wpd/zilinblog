7b851da411ac13b50a4ce3616b74b7dbedba7c91
andishe <a.abbasian@pixaldo.com>
2023-07-08 00:00:01 +0330

    lazy loadimage component

---
+++ src/components/lazyLoadImage/lazyLoadImage.tsx
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import { PostImageProps } from '../../interfaces/PostImageProps'
+
+const LazyLoadImage: React.FC<PostImageProps> = ({ picture }) => {
+  const [loading, setLoading] = useState<boolean>(true)
+  const [currentImage, setCurrentImage] = useState<string>('/samplepost.jpg') // replace this with your placeholder image path
+
+  useEffect(() => {
+    const img = new Image()
+
+    img.src = picture
+    img.onload = () => {
+      setCurrentImage(picture)
+      setLoading(false)
+    }
+  }, [picture])
+
+  return (
+    <div
+      className={`transition-opacity duration-500 ${
+        loading ? 'opacity-50' : 'opacity-100'
+      }`}
+    >
+      <img src={currentImage} alt="" />
+    </div>
+  )
+}
+
+export default LazyLoadImage
