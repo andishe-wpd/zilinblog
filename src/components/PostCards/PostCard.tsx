ae0603517bfc05d5d592f61a8c1982a4779ad4a9
andishe <a.abbasian@pixaldo.com>
2023-07-07 14:18:47 +0330

    post card updated - navigation to post details

--- src/components/PostCards/PostCard.tsx
+++ src/components/PostCards/PostCard.tsx
@@ -1,32 +1,41 @@
 import Button from '@components/button/Button'
 import samplepost from '/samplepost.png'
 import ArrowIcon from '@assets/icons/ArrowIcon'
+import { useNavigate, useParams } from 'react-router-dom'
 const PostCard = () => {
+  const navigate = useNavigate()
+  const params = useParams()
   return (
-    <div className="max-w-[326px] mx-auto">
-      <div className="relative ">
-        <img src={samplepost} alt="" />
-        <div className="absolute gradient-glass-card ">
-          <div className="flex flex-col text-sm	">
-            <span className="font-semibold">Lana Steiner</span>
-            <span>18 Jan 2022</span>
+    <>
+      <div className="max-w-[326px] mx-auto">
+        <div className="relative ">
+          <img src={samplepost} alt="" />
+          <div className="absolute gradient-glass-card ">
+            <div className="flex flex-col text-sm	">
+              <span className="font-semibold">Lana Steiner</span>
+              <span>18 Jan 2022</span>
+            </div>
+            <span className="font-semibold">Developer</span>
           </div>
-          <span className="font-semibold">Developer</span>
         </div>
-      </div>
-      <div className="py-8 ">
-        <div className=" font-semibold text-2xl"> Building your API Stack</div>
-        <div className="text-[#667085]">
-          The rise of RESTful APIs has been met by a rise in tools for creating,
-          testing, and managing them.
+        <div className="py-8 ">
+          <div className=" font-semibold text-2xl">
+            {' '}
+            Building your API Stack
+          </div>
+          <div className="text-[#667085]">
+            The rise of RESTful APIs has been met by a rise in tools for
+            creating, testing, and managing them.
+          </div>
         </div>
+        <Button
+          title={'Read post'}
+          className="flex-row-reverse text-primary hover:bg-transparent"
+          icon={<ArrowIcon className="tranform rotate-135" stroke="#175CD3" />}
+          onClickAction={() => navigate('/' + (params?.page || '1') + '/5')}
+        />
       </div>
-      <Button
-        title={'Read post'}
-        className="flex-row-reverse p-0 text-primary hover:bg-transparent"
-        icon={<ArrowIcon className="tranform rotate-135" stroke="#175CD3" />}
-      />
-    </div>
+    </>
   )
 }
 
