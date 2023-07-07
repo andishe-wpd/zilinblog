dc7cc4340efb750b929d425f49b13a09300563cf
andishe <a.abbasian@pixaldo.com>
2023-07-08 02:10:15 +0330

    deleted zombie code

--- src/components/PostCards/PostCardDetail.tsx
+++ src/components/PostCards/PostCardDetail.tsx
@@ -7,16 +7,10 @@ const PostCardDetail: FC<{ content: Post }> = ({ content }) => {
   return (
     <div className="flex flex-col md:flex-row max-h-[90vh] mx-auto  md:max-w-screen-lg shadow-lg  rounded-lg bg-white">
       <div className="relative w-full h-full sm:min-h-[80vh] md:w-12/12 lg:w-8/12 xl:w-2/3">
-        {/* {content?.image ? <LazyLoadImage className:{w-full h-full object-cover rounded-l-lg} picture={content.image} /> : null} */}
         <LazyLoadImage
           className={'w-full h-full object-cover rounded-l-lg'}
           picture={content.image}
         />
-        {/* <img
-          src={content.image}
-          alt="description"
-          className="w-full h-full object-cover rounded-l-lg"
-        /> */}
         <div className="absolute gradient-glass-card rounded-bl-lg">
           <div className="flex flex-col text-sm	">
             <span className="font-semibold">{content?.author}</span>
