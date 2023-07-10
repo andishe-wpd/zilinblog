f927c57f09ab2ef641ae72ea8157f823bd1f473f
andishe <a.abbasian@pixaldo.com>
2023-07-10 07:53:39 +0330

    added token validator to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,7 @@
 import PostCards from '@components/PostCards/PostCards'
 import { useParams, useNavigate } from 'react-router-dom'
 import Modal from '@components/modal/Modal'
+import LoginModal from '@components/login/LoginModal'
 const Home = () => {
   const navigate = useNavigate()
   const params = useParams()
